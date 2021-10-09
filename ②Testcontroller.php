6115000932703f016630cecee44bd62537b871ac
ta棚橋章 <akira59631947@gmail.com>
2021-10-10 08:22:46 +0900

    first-commit

---
+++ ②Testcontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class TestController extends Controller
+{
+    public function index($room){
+        return '部屋番号は'.$room.'です';
+    }
+
+}
