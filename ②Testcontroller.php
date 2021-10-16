6860bbb7a955c4954b778f040a6450ef0f985d51
ta棚橋章 <akira59631947@gmail.com>
2021-10-16 13:04:18 +0900

    last

--- ②Testcontroller.php
+++ ②Testcontroller.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-
 class TestController extends Controller
 {
-    public function index($room){
-        return '部屋番号は'.$room.'です';
+    public function index($text = "建物です")
+    {
+        return $text;
     }
-
+    public function room($texts = "部屋番号")
+    {
+        return "部屋番号は".$texts."です";
+    }
 }
--- ②web.php
+++ ②web.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\TestController;
 
 
 Route::get('/building',[TestController::class,'index']);
-Route::get('/building/{room}',[TestController::class,'index']);
+Route::get('/building/{room}',[TestController::class,'room']);
